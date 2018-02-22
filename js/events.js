c20fba9f59de16e2d1f5674a3bac452968c987b7
Thomas Casady <tscasady@gmail.com>
2018-02-22 19:22:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt(){
 
 function pressIt(){
   $().on('keydown', function(key){
-    if (key.which === 71){
+    if (key.which == 71){
       alert('g has been pressed')
     }
   })
