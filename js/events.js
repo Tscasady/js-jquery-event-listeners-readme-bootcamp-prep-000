cb623c95ff4af84f114ea350a4a18d2550cd273f
Thomas Casady <tscasady@gmail.com>
2018-02-22 18:33:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,11 +10,17 @@ function frameIt(){
 }
 
 function pressIt(){
-  .on('keydown', function(key){
-    if (key === 71){
-      
+  $().on('keydown', function(key){
+    if (key.which === 71){
+      alert('g has been pressed')
     }
   })
+}
+
+function submitIt(){
+  .on('submit',){
+    alert('Your form is going to be submitted now.')
+  }
 }
 
 $(document).ready(function(){
